cd2b761923a38cf416cef90a8be9e3f8278f3a1e
aragaoItalo <italoaragao23@gmail.com>
2024-10-09 20:43:15 -0300

    Add validações e tratamentos de erros

--- backend/src/models/cliente.js
+++ backend/src/models/cliente.js
@@ -5,24 +5,52 @@ const sequelize = require('../config/db');
 const Cliente = sequelize.define('Cliente', {
     nome: {
         type: DataTypes.STRING,
-        allowNull: false
+        allowNull: false,
+        validate: {
+            notEmpty: true //Aqui verificamos se o campo está vazio.
+        }
     },
     email: {
         type: DataTypes.STRING,
         allowNull: false,
-        unique: true
+        unique: true,
+        validate: {
+            isEmail: true, //Aqui verificamos se o formato de email é válido
+            notEmpty: true
+        }
     },
     senha: {
         type: DataTypes.STRING,
-        allowNull: false
+        allowNull: false,
+        validate: {
+            len: {
+                args: [6, 30], //Min e Máx
+                msg: "A senha deve ter entre 6 e 30 caracteres."
+            },
+            notEmpty: true
+        }
     },
     endereco: {
         type: DataTypes.STRING,
-        allowNull: false
+        allowNull: false,
+        validate: {
+            len: {
+                args: [10, 255],
+                msg: "O endereço deve ter entre 10 e 255 caracteres."
+            },
+            notEmpty: true
+        }
     },
     telefone: {
         type: DataTypes.STRING,
-        allowNull: false
+        allowNull: false,
+        validate: {
+            len: {
+                args: [10, 15],
+                msg: "O telefone deve ter entre 10 e 15 caracteres"
+            },
+            notEmpty: true
+        }
     }
 }, { 
     hooks: {
