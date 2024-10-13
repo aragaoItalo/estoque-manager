4f079e9d343f7b142d7ebc64a3259105eeea3a17
aragaoItalo <italoaragao23@gmail.com>
2024-10-13 00:18:17 -0300

    Subindo novas rotas e controladores

--- backend/src/routes/clienteRoutes.js
+++ backend/src/routes/clienteRoutes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const clienteController = require('../controllers/clienteController');
 
-
 router.post('/', clienteController.createCliente);
 router.get('/', clienteController.getAllClientes);
 router.get('/:id', clienteController.getClienteById);
---
+++ backend/src/routes/itemPedidoRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router;
+const itemPedidoController = require('../controllers/itemPedidoController');
+
+router.post('/:pedidoId', itemPedidoController.addItemToPedido);
+router.put('/:itemId', itemPedidoController.updateItemInPedido);
+router.delete('/:itemId', itemPedidoController.removeItemFromPedido);
+
+module.exports = router;
